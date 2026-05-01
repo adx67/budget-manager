525c06a5da67d7796fac4b0622e4ac97a22cb698
adx67 <adamlachkheb2@gmail.com>
2026-05-01 17:21:47 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'budget-manager-v2';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './icon-192.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
